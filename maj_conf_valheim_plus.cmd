bf2da08a0677c3a42120976a3e218dddc04f2aa7
lud0vicb <lud0vicb@zoho.eu>
2021-03-01 23:39:16 +0100

    Update maj_conf_valheim_plus.cmd

--- maj_conf_valheim_plus.cmd
+++ maj_conf_valheim_plus.cmd
@@ -6,6 +6,6 @@ set quickconnect="%valheim%\BepInEx\config\quick_connect_servers.cfg"
 set quickdll="%valheim%\BepInEx\plugins\QuickConnect.dll"
 
 curl -o %gameconf% https://raw.githubusercontent.com/lud0vicb/valheim-conf/main/Valheim/BepInEx/config/valheim_plus.cfg
-copy %gameconf% %serverconf% 
+curl -o %serverconf% https://raw.githubusercontent.com/lud0vicb/valheim-conf/main/Valheim/BepInEx/config/valheim_plus.cfg
 curl -o %quickconnect% https://raw.githubusercontent.com/lud0vicb/valheim-conf/main/Valheim/BepInEx/config/quick_connect_servers.cfg
 curl -o %quickdll% https://raw.githubusercontent.com/lud0vicb/valheim-conf/main/Valheim/BepInEx/plugins/QuickConnect.dll
