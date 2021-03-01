9d7890ce0fb8ad17b3a92242252e6fcb71c1a788
lud0vicb <lud0vicb@zoho.eu>
2021-03-01 23:23:43 +0100

    Delete maj_server_valheim_plus.cmd

--- maj_server_valheim_plus.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set valheim="e:\Steam\SteamApps\common\Valheim"
-set server="e:\Steam\SteamApps\common\Valheim dedicated server"
-set plusconf="BepInEx\config\valheim_plus.cfg"
-set srcconf="C:\Users\lud0vicb\Documents\valheim\Valheim\BepInEx\config"
-
-copy %srcconf% %server%\%plusconf%
-copy %srcconf% %valheim%\%plusconf%
