69210f13abd427a1941a380da70e2e20f30e3d16
Ludovic Bellier <ludovic.bellier@grenoble-inp.fr>
2021-03-01 19:42:44 +0100

    ajout quickconnect

--- maj_conf_valheim_plus.cmd
+++ maj_conf_valheim_plus.cmd
@@ -3,6 +3,9 @@
 set valheim="e:\Steam\SteamApps\common\Valheim"
 
 set gameconf="%valheim%\BepInEx\config\valheim_plus.cfg"
-set destconf="%appdata%\..\..\Downloads\valheim_plus.cfg"
-curl -o %destconf% https://raw.githubusercontent.com/lud0vicb/valheim-conf/main/Valheim/BepInEx/config/valheim_plus.cfg
-copy %destconf% %gameconf%
+set quickconnect="%valheim%\BepInEx\config\quick_connect_servers.cfg"
+set quickdll="%valheim%\BepInEx\plugins\QuickConnect.dll"
+
+curl -o %gameconf% https://raw.githubusercontent.com/lud0vicb/valheim-conf/main/Valheim/BepInEx/config/valheim_plus.cfg
+curl -o %quickconnect% https://raw.githubusercontent.com/lud0vicb/valheim-conf/main/Valheim/BepInEx/config/quick_connect_servers.cfg
+curl -o %quickdll% https://github.com/lud0vicb/valheim-conf/raw/main/Valheim/BepInEx/plugins/QuickConnect.dll
