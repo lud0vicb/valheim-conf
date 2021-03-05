c1818c192e1626bb74b4744826f1558d361274c4
Ludovic Bellier <ludovic.bellier@grenoble-inp.fr>
2021-03-05 20:00:51 +0100

    install conf Fri Mar  5 20:00:51 CET 2021

--- install_conf.sh
+++ install_conf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 VER=9.3
-cp valheim_plus_$VER.cfg Valheim/BepInEx/config/
-cp valheim_plus_$VER.cfg Valheim\ dedicated\ server/BepInEx/config/
+cp valheim_plus_$VER.cfg Valheim/BepInEx/config/valheim_plus.cfg
+cp valheim_plus_$VER.cfg Valheim\ dedicated\ server/BepInEx/config/valheim_plus.cfg
 
 git commit  -m "install conf $(date)" .
 git push
