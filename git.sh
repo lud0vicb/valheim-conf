ad3d278ba9a8af3e8070e73c5e29107ca38d33b8
Ludovic Bellier <ludovic.bellier@grenoble-inp.fr>
2021-03-07 10:50:50 +0100

    Sun Mar  7 10:50:50 CET 2021

--- git.sh
+++ git.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+printf "préparation\n---\n"
 git status
 git add -f Valheim/BepInEx/plugins/
 git add -f Valheim/BepInEx/config/
+git add -f *.sh
+git add -f *.cfg
+printf "ajouts fait\n---\n"
 git status
 git commit  -m "$(date)" .
 git push
+printf "post\n---\n"
+git status
 
