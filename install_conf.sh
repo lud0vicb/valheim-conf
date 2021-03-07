5c97acbade8c3dc86ad443f0b7164d0613fafebd
Ludovic Bellier <ludovic.bellier@grenoble-inp.fr>
2021-03-07 10:27:43 +0100

    Sun Mar  7 10:27:43 CET 2021

--- install_conf.sh
+++ install_conf.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-set -x
 VER=9.3
 cp valheim_plus_$VER.cfg Valheim/BepInEx/config/valheim_plus.cfg
 cp valheim_plus_$VER.cfg Valheim\ dedicated\ server/BepInEx/config/valheim_plus.cfg
-
-git commit  -m "install conf $(date)" .
-git push
-
+ls -l Valheim/BepInEx/config/valheim_plus.cfg
+ls -l Valheim\ dedicated\ server/BepInEx/config/valheim_plus.cfg
--- save_world.sh
+++ save_world.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -x
 
 cp -R /mnt/c/Users/lud0vicb/AppData/LocalLow/IronGate/Valheim ./backup/
 
+git add -f ./backup
+git status
 git commit  -m "backup world $(date)" ./backup
 git push
 
