cfd7d384576682d255344a08763bdcafc1fc2cbd
Ludovic Bellier <ludovic.bellier@grenoble-inp.fr>
2021-03-09 15:34:59 +0100

    Tue Mar  9 15:34:59 CET 2021

--- Valheim dedicated server/start_headless_server.bat
+++ Valheim dedicated server/start_headless_server.bat
@@ -6,5 +6,5 @@ echo "Starting server PRESS CTRL-C to exit"
 REM Tip: Make a local copy of this script to avoid it being overwritten by steam.
 REM NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 REM NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-valheim_server -nographics -batchmode -name "Glorom" -port 2456 -world "GLorom" -password "les amis de glorom"
+valheim_server -nographics -batchmode -name "Glorom" -port 2456 -world "Glorom" -password "les amis de glorom"
 
