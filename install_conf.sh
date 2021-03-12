bd55d9229bd49a132e44332c8cecf8efe29d4c64
Ludovic Bellier <ludovic.bellier@grenoble-inp.fr>
2021-03-13 00:56:37 +0100

    Sat Mar 13 00:56:37 CET 2021

--- install_conf.sh
+++ install_conf.sh
@@ -6,5 +6,6 @@ rm Valheim\ dedicated\ server/BepInEx/plugins/*.dll
 cp Valheim/BepInEx/config/*.cfg  Valheim\ dedicated\ server/BepInEx/config/
 cp Valheim/BepInEx/plugins/*.dll  Valheim\ dedicated\ server/BepInEx/plugins/
 cp valheim_plus_$VER.cfg Valheim\ dedicated\ server/BepInEx/config/valheim_plus.cfg
+cp assembly_valheim.dll Valheim\ dedicated\ server/valheim_server_Data/Managed
 ls -l Valheim/BepInEx/config/valheim_plus.cfg| cut -d ' ' -f6-
 ls -l Valheim\ dedicated\ server/BepInEx/config/valheim_plus.cfg| cut -d ' ' -f6-
