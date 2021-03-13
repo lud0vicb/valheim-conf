4a7b5b84c059191c4bb469795487c5a31ef0d86c
Ludovic Bellier <ludovic.bellier@grenoble-inp.fr>
2021-03-14 00:59:02 +0100

    Sun Mar 14 00:59:02 CET 2021

--- download_glorom.cmd
+++ download_glorom.cmd
@@ -1,7 +1,10 @@
-set valheim="e:\Steam\SteamApps\common\Valheim"
+set valheim=e:\Steam\SteamApps\common\Valheim
+del /f /s /q valheim-conf-cfg-dll
 curl -o valheim-glorom.zip -LJO https://github.com/lud0vicb/valheim-conf/archive/cfg-dll.zip
 tar -xf valheim-glorom.zip
 cd valheim-conf-cfg-dll
+del /f %valheim%\BepInEx\config\*.cfg
+del /f %valheim%\BepInEx\plugins\*.dll
 copy Valheim\BepInEx\config\*.cfg %valheim%\BepInEx\config\
 copy Valheim\BepInEx\plugins\*.dll %valheim%\BepInEx\plugins\
 explorer %valheim%
--- install_conf.sh
+++ install_conf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 VER=9.5
 cp valheim_plus_$VER.cfg Valheim/BepInEx/config/valheim_plus.cfg
-rm Valheim\ dedicated\ server/BepInEx/config/*.cfg
-rm Valheim\ dedicated\ server/BepInEx/plugins/*.dll
-cp Valheim/BepInEx/config/*.cfg  Valheim\ dedicated\ server/BepInEx/config/
-cp Valheim/BepInEx/plugins/*.dll  Valheim\ dedicated\ server/BepInEx/plugins/
+#rm Valheim\ dedicated\ server/BepInEx/config/*.cfg
+#rm Valheim\ dedicated\ server/BepInEx/plugins/*.dll
+#cp Valheim/BepInEx/config/*.cfg  Valheim\ dedicated\ server/BepInEx/config/
+#cp Valheim/BepInEx/plugins/*.dll  Valheim\ dedicated\ server/BepInEx/plugins/
 cp valheim_plus_$VER.cfg Valheim\ dedicated\ server/BepInEx/config/valheim_plus.cfg
 cp assembly_valheim.dll Valheim\ dedicated\ server/valheim_server_Data/Managed
 ls -l Valheim/BepInEx/config/valheim_plus.cfg| cut -d ' ' -f6-
