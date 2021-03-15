97c83cd9f15e9f3cc0be9b4dc9b0d8a0dbb8d033
Ludovic Bellier <ludovic.bellier@grenoble-inp.fr>
2021-03-15 15:49:17 +0100

    Mon Mar 15 15:49:17 CET 2021

--- install_conf.sh
+++ install_conf.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -x
+# conf plus client + server
 VER=9.5
 cp valheim_plus_$VER.cfg Valheim/BepInEx/config/valheim_plus.cfg
-#rm Valheim\ dedicated\ server/BepInEx/config/*.cfg
-#rm Valheim\ dedicated\ server/BepInEx/plugins/*.dll
-#cp Valheim/BepInEx/config/*.cfg  Valheim\ dedicated\ server/BepInEx/config/
-#cp Valheim/BepInEx/plugins/*.dll  Valheim\ dedicated\ server/BepInEx/plugins/
 cp valheim_plus_$VER.cfg Valheim\ dedicated\ server/BepInEx/config/valheim_plus.cfg
+
+# hack server pour data sent 60=>600
 cp assembly_valheim.dll Valheim\ dedicated\ server/valheim_server_Data/Managed
-ls -l Valheim/BepInEx/config/valheim_plus.cfg| cut -d ' ' -f6-
-ls -l Valheim\ dedicated\ server/BepInEx/config/valheim_plus.cfg| cut -d ' ' -f6-
+
+# plugin swpawn that client + serveur / plugin + conf
+cp Valheim.SpawnThat.dll Valheim\ dedicated\ server/BepInEx/plugins/
+cp Valheim.SpawnThat.dll Valheim/BepInEx/plugins/
+cp spawn_that.world_spawners_advanced.cfg Valheim\ dedicated\ server/BepInEx/config/
+cp spawn_that.world_spawners_advanced.cfg Valheim/BepInEx/config/
