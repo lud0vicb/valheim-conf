59fd01852825f7382e4e86a2475cef3d850da00b
Ludovic Bellier <ludovic.bellier@grenoble-inp.fr>
2021-03-19 16:20:41 +0100

    Fri Mar 19 16:20:41 CET 2021

--- save_world.sh
+++ save_world.sh
@@ -2,6 +2,7 @@
 
 cd /mnt/d/zBackup/valheim-backup/
 #tar zvcf Valheim.tgz /mnt/c/Users/lud0vicb/AppData/LocalLow/IronGate/Valheim/
+rm valheim-backup.z*
 zip -r -s 10m valheim-backup.zip /mnt/c/Users/lud0vicb/AppData/LocalLow/IronGate/Valheim/
 git add .
 git status
