babe0457261d7bc9a39cf39cb208656cb6d99636
Ludovic Bellier <ludovic.bellier@grenoble-inp.fr>
2021-03-20 21:27:51 +0100

    Sat Mar 20 21:27:51 CET 2021

--- download_glorom.cmd
+++ download_glorom.cmd
@@ -2,9 +2,9 @@ set valheim=e:\Steam\SteamApps\common\Valheim
 del /f /s /q valheim-conf-cfg-dll
 curl -o valheim-glorom.zip -LJO https://github.com/lud0vicb/valheim-conf/archive/cfg-dll.zip
 tar -xf valheim-glorom.zip
-cd valheim-conf-cfg-dllr
-del /f /s %valheim%\BepInEx\config\
-del /f /s %valheim%\BepInEx\plugins\
-xcopy /s Valheim\BepInEx\config\ %valheim%\BepInEx\
-xcopy /s Valheim\BepInEx\plugins\ %valheim%\BepInEx\
+cd valheim-conf-cfg-dll
+del /f /s /q %valheim%\BepInEx\config
+del /f /s /q %valheim%\BepInEx\plugins
+robocopy /s Valheim\BepInEx\config %valheim%\BepInEx\config
+robocopy /s Valheim\BepInEx\plugins %valheim%\BepInEx\plugins
 explorer %valheim%
