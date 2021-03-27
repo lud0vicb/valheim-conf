b5dad396a68406437b465fb6ccabbd8eea2b9397
Ludovic Bellier <ludovic.bellier@grenoble-inp.fr>
2021-03-27 10:32:01 +0100

    Sat Mar 27 10:32:01 CET 2021

--- install_conf.sh
+++ install_conf.sh
@@ -10,12 +10,12 @@ cp valheim_plus_$VER.cfg Valheim\ dedicated\ server/BepInEx/config/valheim_plus.
 #cp assembly_valheim.dll Valheim\ dedicated\ server/valheim_server_Data/Managed 
 
 # plugin swpawn that client + serveur / plugin + conf
-# cp Valheim.SpawnThat.dll Valheim\ dedicated\ server/BepInEx/plugins/
-# cp Valheim.SpawnThat.dll Valheim/BepInEx/plugins/
-# cp spawn_that.world_spawners_advanced.cfg Valheim\ dedicated\ server/BepInEx/config/
-# cp spawn_that.world_spawners_advanced.cfg Valheim/BepInEx/config/
-# cp spawn_that.simple.cfg Valheim/BepInEx/config/
-# cp spawn_that.simple.cfg Valheim\ dedicated\ server/BepInEx/config/
+cp Valheim.SpawnThat.dll Valheim\ dedicated\ server/BepInEx/plugins/
+cp Valheim.SpawnThat.dll Valheim/BepInEx/plugins/
+cp spawn_that.world_spawners_advanced.cfg Valheim\ dedicated\ server/BepInEx/config/
+cp spawn_that.world_spawners_advanced.cfg Valheim/BepInEx/config/
+cp spawn_that.simple.cfg Valheim/BepInEx/config/
+cp spawn_that.simple.cfg Valheim\ dedicated\ server/BepInEx/config/
 
 # loot epic
 # cp ExtendedItemDataFramework.dll Valheim/BepInEx/plugins/
